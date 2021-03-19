9eeceb4919ba99cec0b19703b112b912039643bd
Papa Daco <papadaco@PapaDaco.local>
2021-03-19 17:14:53 -0400

    S13 Native Device Features init up-to map use

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './add_place_screen.dart';
+import '../providers/places.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<Places>(context, listen: false).fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+            : Consumer<Places>(
+                child: Center(
+                  child: Text('No places yet. Start adding some..'),
+                ),
+                builder: (ctx, places, ch) => places.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: places.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(places.items[i].image),
+                          ),
+                          title: Text(
+                            places.items[i].title,
+                          ),
+                          onTap: () {
+                            // go to details
+                          },
+                        ),
+                      ),
+              ),
+      ),
+    );
+  }
+}
